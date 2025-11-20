fecf0ac06e5034630a3fa5f0bc951d1825c2d694
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2025-11-20 16:29:46 -0300

    Add HistoryAccessDeniedException with HTTP 403 response status

--- services/history-service/src/main/java/br/com/fiap/clinic/history/exception/HistoryAccessDeniedException.java
+++ services/history-service/src/main/java/br/com/fiap/clinic/history/exception/HistoryAccessDeniedException.java
@@ -1,5 +1,9 @@
 package br.com.fiap.clinic.history.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class HistoryAccessDeniedException extends RuntimeException {
     public HistoryAccessDeniedException(String message) {
         super(message);
